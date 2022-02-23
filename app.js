39d3eac0910e611eeef388a4b8600cddceef6e89
gary <gary.white@lodgit.com>
2022-02-23 13:37:15 +0100

    code working

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const arr = N => [ /* the numbers 0 to N-1 */ ];
+// const arr = N => [ /* the numbers 0 to N-1 */ ];
+
+function arr(N){
+    var myArr = [];
+    for (var i = 0; i < N; i++){
+        myArr.push(i);
+    }
+    return myArr;
+}
 
 console.log(arr(4)) // [0,1,2,3]
